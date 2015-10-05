111b26c61facd283c0f22742105e83bb1c3da614
suxaryk <suxaryk@ukr.net>
2015-10-05 23:03:21 +0200

    fix removeCash

--- src/cafe/model/Order.java
+++ src/cafe/model/Order.java
@@ -16,10 +16,11 @@ import java.util.Map;
  */
 public class Order {   
     private int cookCount;
+    private int drinkCount;    
     private int orderSum;
     private boolean payed;
     private ArrayList<OrderItem> items = new ArrayList<>();
-   // private ArrayList<Ingredient> ingredients = new ArrayList<>();
+
     
 
      
@@ -27,33 +28,50 @@ public class Order {
     public int getOrderSum() {
         int sum = 0;
         for (OrderItem orderItem : items) {
-            sum += orderItem.getSum();            
+            sum += orderItem.getSum();
         }
-        return orderSum = sum;
+        if (orderSum < 0) {                     
+            return orderSum;
+        } else {
+            return orderSum = sum;
+        }
     }
 
-    public int getCookCount() {        
-        for (OrderItem checkItem : items) {
-            if (checkItem.getDish().isCook()) {
-                cookCount+=checkItem.getCount();                  
+    public int getDrinkCount() {
+        for (OrderItem orderItem : items) {
+            if (orderItem.getDish().isDrink()) {
+                drinkCount += orderItem.getCount();
             }
         }
-        return cookCount;
+        System.out.println("drinkCount" + drinkCount);
+        return drinkCount;
+
     }
+
+    public int getCookCount() { 
+            for (OrderItem orderItem : items) {
+                if (orderItem.getDish().isCook()) {
+                    cookCount += orderItem.getCount();
+                }
+            }                     
+//        }        
+        System.out.println("cookCount" + cookCount);
+        return cookCount;
+    }  
 
     public void setCookCount(int cookCount) {
         this.cookCount = cookCount;
     }
 
-    public void setTotalsum(int orderSum) {
+    public void setOrderSum(int orderSum) {
         this.orderSum = orderSum;
     }
 
-    public ArrayList<OrderItem> getCheckList() {
+    public ArrayList<OrderItem> getItems() {
         return items;
     }
 
-    public void setCheckList(ArrayList<OrderItem> checkList) {
+    public void setItems(ArrayList<OrderItem> checkList) {
         this.items = checkList;
     }
     
